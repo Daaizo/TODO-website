d7ce7876f29ca546921ac2950d10e2fa637fdf89
Daniel Charlak <danielcharlak@wp.pl>
2022-06-07 22:09:13 +0200

    Huge upddate
    New front page, slider on it and data downloaded from server in json format. Some media requests and css done and many many other tasks have been done.

---
+++ JS/commentsFetch.js
@@ -0,0 +1,39 @@
+//Skrypt z Fetch API
+//sprawdź czy DOM został załadowany:
+
+document.addEventListener("DOMContentLoaded", function() {
+    $(".slick-arrow").css("z-index","100");
+    $(".slick-prev").css("z-index","100");
+    $("#comments").css("width","100%");
+    $("#comments").css("z-index","10");
+    
+    
+	fetch("https://jsonplaceholder.typicode.com/comments/")
+    .then(response => response.json())
+    .then(json => printData(json));
+   
+    function printData(json){
+        console.log(json);
+        var lista = json;
+        var el=document.getElementById("comments");
+        var str="";
+        if (lista===null) el.innerHTML=str+"<tr><td>Brak danych </td></tr>";
+        else {
+            //lista.lenght - wszystkie komentarze ale jest ich 500 wieć tylko 20 pierwszych
+            for(i=0;i<20;i++) {
+                str+= "\n<span class='comment'>";
+
+                str+= "<table><tbody><tr><td id='tableTitle' colspan='2'> Numer komentarza   "+ (i+1) +"</td></tr>";
+                str+= "<tr><td> Email </td><td>" +lista[i].email +"  "+"</td></tr>";
+                str+= "<tr><td> Tytuł </td><td>" +lista[i].name +"</td></tr>";
+                str+= "<tr><td colspan='2'>" +lista[i].body +"</td></tr></tbody></table>";
+                 
+                str+= "\n</span>";
+               
+            }
+            $("#comments").slick('slickAdd', str);
+           
+        } 
+     
+    }
+});
---
+++ JS/slider.js
@@ -0,0 +1,52 @@
+
+ $(document).on('ready', function() {
+	
+ 
+    $('.single-item').slick({
+      infinite: true,
+      autoplay: false,
+      autoplaySpeed: 1500,
+      speed: 600,
+      slidesToShow: 3,
+      slidesToScroll: 3,
+      pauseOnHover: true,
+      draggable: true,
+      dots: true,
+      prevArrow: true,
+      nextArrow: true,
+      
+      responsive: [
+        {
+          breakpoint: 1000,
+          settings: {
+            slidesToShow: 2,
+            slidesToScroll: 2,
+            infinite: true,
+            dots: true,
+            vertical: false,
+            
+          }
+        },
+        {
+          breakpoint: 600,
+          settings: {
+            slidesToShow: 1,
+            slidesToScroll: 1,
+            dots: false
+          }
+        },
+        {
+          breakpoint: 350,
+          settings:  "unslick",
+          
+        }
+        // You can unslick at a given breakpoint now by adding:
+        // settings: "unslick"
+        // instead of a settings object
+      ]
+    });
+    $(window).resize(function(){
+      $('.single-item')[0].slick.refresh();
+    });
+
+});
