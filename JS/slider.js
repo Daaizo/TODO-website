8e1c80fa6af4014e1283fc4a3c37b9519bfa43c1
Daniel Charlak <danielcharlak@wp.pl>
2022-06-25 16:53:08 +0200

    slider init

---
+++ JS/slider.js
@@ -0,0 +1,48 @@
+$(document).on("ready", function () {
+  $(".single-item").slick({
+    infinite: true,
+    autoplay: false,
+    autoplaySpeed: 1500,
+    speed: 600,
+    slidesToShow: 3,
+    slidesToScroll: 3,
+    pauseOnHover: true,
+    draggable: true,
+    dots: true,
+    prevArrow: true,
+    nextArrow: true,
+
+    responsive: [
+      {
+        breakpoint: 1000,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2,
+          infinite: true,
+          dots: true,
+          vertical: false,
+        },
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+          dots: false,
+        },
+      },
+      {
+        breakpoint: 350,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+          dots: false,
+        },
+      },
+    ],
+  });
+
+  $(window).resize(function () {
+    $(".single-item")[0].slick.refresh();
+  });
+});
