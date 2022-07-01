c3f3071a8590f50e23cdb4d24661f63d2e3e10e0
Daniel Charlak <danielcharlak@wp.pl>
2022-07-01 20:28:48 +0200

    feat: searchar, todo id. fix: edition and deletion
    I finally managed to fix the search bar. Now when text is found, it is highlighted in one of the background colors. The most difficult task was to disable special regex characters such as '?' or '.', I managed to achieve this by checking if any of these special characters are present in the text being typed and disabling them via the escape character. Now it works perfectly. Also now all todo id's are counted based on all todo list lengths - a new todo is the last item so it gets an id of length+1 and never changes. If a todo is deleted, the id will not change. Whole class is refactored

--- JS/Todo.js
+++ JS/Todo.js
@@ -1,8 +1,7 @@
 class Todo {
   static emptyListText =
-    "<span class='bigText' style='grid-column: 1/ span2;'>There is nothing here ... Add a new TODO!";
+    "<span class='bigText spannedRow'>There is nothing here ... Add a new TODO!";
   constructor(login) {
-    this.storageName = "TODOs";
     this.setUserLogin(login);
   }
 
@@ -15,174 +14,203 @@ class Todo {
   }
 
   addTODOtoStorage(data) {
-    var list = JSON.parse(localStorage.getItem(this.storageName));
+    var list = getLocalStorageWithTodos();
     if (list == null || list.length == 0) {
       list = [];
       data.id = 0;
     } else data.id = list[list.length - 1].id + 1;
     list.push(data);
-    localStorage.setItem(this.storageName, JSON.stringify(list));
+    updateLocalStorageWithTodos(list);
     createAlert("success", "The new TODO has been successfully saved");
   }
-}
-function removoAllUserTODOsfromStorage(login) {
-  var list = JSON.parse(localStorage.getItem("TODOs"));
-  for (let i = 0; i < list.length; i++) {
-    if (list[i].userLogin == login) {
-      if (list.length != 1) {
-        list.splice(i, 1);
-        i = 0;
-      } else list.pop();
-    }
-  }
-  localStorage.setItem("TODOs", JSON.stringify(list));
-}
-
-function displayTODOs(login) {
-  var lista = JSON.parse(localStorage.getItem("TODOs"));
-  var el = document.getElementById("todos");
-  var str = "";
-  if (lista == null) el.innerHTML = str + Todo.emptyListText;
-  else {
-    for (i = 0; i < lista.length; i++) {
-      if (lista[i].userLogin == login) {
-        if (lista[i].type == "Critical")
-          str +=
-            "<table class='todoRow'><tr id='last'><td><h5 style='background-color:yellow'>";
-        else str += "<table class='todoRow'><tr id='last'><td><h5>";
-        str +=
-          "TODO number " +
-          lista[i].id +
-          " </h5><span class='boxTODO' >  <button onclick='deleteTODO(" +
-          i +
-          ")' class='cancelButton'> delete TODO</button> " +
-          "  <button onclick='editTODO(" +
-          i +
-          ")'>edit TODO</button> " +
-          "</span></td></tr>";
-        str +=
-          "<tr><td> Title : <span class='todoText'>" +
-          lista[i].title +
-          "</span></td></tr>";
-        str +=
-          "<tr ><td> Description  :  <span class='todoText'>" +
-          lista[i].description +
-          "</span></td></tr>";
-
-        str +=
-          "<tr><td> Todo type :  <span class='todoText'>" +
-          lista[i].type +
-          "</span></td></tr>";
-        str +=
-          "<tr><td> Tags :  <span class='todoText'>" +
-          lista[i].important +
-          "</span></td></tr></table>";
+  getUserTodos() {
+    var lista = getLocalStorageWithTodos();
+    var userTodos = [];
+    if (lista != null) {
+      for (let i = 0; i < lista.length; i++) {
+        if (lista[i].userLogin == this.userLogin) {
+          userTodos.push(lista[i]);
+        }
       }
     }
-    if (str == "") el.innerHTML = Todo.emptyListText;
-    else el.innerHTML = str;
+    return userTodos;
+  }
+
+  removoAllUserTODOsfromStorage(login) {
+    var list = getLocalStorageWithTodos();
+    for (let i = 0; i < list.length; i++) {
+      if (list[i].userLogin == login) {
+        if (list.length != 1) {
+          list.splice(i, 1);
+          i = 0;
+        } else list.pop();
+      }
+    }
+    updateLocalStorageWithTodos(list);
+  }
+  dislayUserTodos() {
+    var lista = this.getUserTodos();
+    var el = $("#todos");
+    var str = "";
+    if (lista == null || lista.length == 0) {
+      el.html(str + Todo.emptyListText);
+      return;
+    } else {
+      for (let i = 0; i < lista.length; i++) {
+        str += printTodo(lista[i]);
+      }
+      el.html(str);
+    }
+  }
+  searchForTODO() {
+    var todoList = this.getUserTodos();
+    var input = $("#searchBar");
+    var el = $("#todos");
+    var finalHtmlString = "";
+    if (todoList.length == 0 || input.val().length == 0) {
+      //there is no todos or searchbar is empty
+      this.dislayUserTodos();
+      return false;
+    }
+
+    //replacing all special characters with \ ex."?" -> "/?
+    //$&	inserts the whole match in .replace function
+    var val = $("#searchBar")
+      .val()
+      .replace(/[!"#$%&'()*+,.\/:;<=>?@[\\\]^`{|}~]/gi, "\\$&");
+    const regex = new RegExp(val, "gi");
+    for (let i = 0; i < todoList.length; i++) {
+      if (
+        todoList[i].title.match(regex) ||
+        regex.test(todoList[i].id) ||
+        todoList[i].description.match(regex) ||
+        todoList[i].type.match(regex) ||
+        todoList[i].important.match(regex)
+      ) {
+        finalHtmlString += printTodo(todoList[i]);
+      }
+    }
+
+    if (finalHtmlString == "")
+      el.html("<span class='bigText spannedRow'> Nothing found </span> ");
+    else {
+      el.html(finalHtmlString);
+      $(".todoText:contains(" + input.val() + ")").each(function () {
+        $(this).html(
+          $(this)
+            .text()
+            .replace(regex, `<span class="regexMarkedText">$&</span>`)
+        );
+      });
+    }
   }
 }
-function editTODO(i) {
+// constains non case sens  https://css-tricks.com/snippets/jquery/make-jquery-contains-case-insensitive/
+$.expr[":"].contains = $.expr.createPseudo(function (arg) {
+  return function (elem) {
+    return $(elem).text().toUpperCase().indexOf(arg.toUpperCase()) >= 0;
+  };
+});
+
+function getLocalStorageWithTodos() {
+  return JSON.parse(localStorage.getItem("TODOs"));
+}
+function updateLocalStorageWithTodos(listOfTodos) {
+  localStorage.setItem("TODOs", JSON.stringify(listOfTodos));
+}
+function findTodoWithId(id) {
+  var listOfTodos = getLocalStorageWithTodos();
+  var returnValue = {};
+  for (let i = 0; i < listOfTodos.length; i++) {
+    if (listOfTodos[i].id == id) {
+      returnValue.todo = listOfTodos[i];
+      returnValue.todoPos = i;
+      return returnValue;
+    }
+  }
+}
+
+function editTODO(todoNumber) {
+  var object = findTodoWithId(todoNumber);
+  var todoToedit = object.todo;
   var htmlData =
     "<div style='width: 100%;text-align: left;'><h1 class='bigText'> Edit TODO </h1> " +
     $("#addTodoTable").html() +
     " </div></br><button class='submitButton' id='acceptEdit' onclick='saveEditedTODO(" +
-    i +
+    object.todoPos +
     ")'>save edited todo</button> <button id='notAcceptEdit' class='cancelButton'>cancel</button>";
   fullScreenAlert(htmlData, "#fullScrWraper");
-  var list = JSON.parse(localStorage.getItem("TODOs"));
-  $(".todoTitle").val(list[i].title);
-  $("#description").val(list[i].description);
-  const todoType = checkRadio("TODOtype");
-  console.log(checkRadio("TODOtype"));
-  console.log(todoType);
-  if (todoType == "Not very important") $("input:radio[name=TODOtype]")[0].checked = true;
-  else $("input:radio[name=TODOtype]")[1].checked = true;
 
+  $(".todoTitle").val(todoToedit.title);
+  $("#description").val(todoToedit.description);
+  if (todoToedit.type == "Not very important")
+    $("[name=TODOtype]")[0].checked = true;
+  else $("input:radio[name=TODOtype]")[1].checked = true;
+  changeCheckboxValue(String(todoToedit.important));
   createAlert(
     "info",
     "Correct the data or enter new data and press the green 'save edited data' button"
   );
 }
+
 function saveEditedTODO(i) {
-  var lista = JSON.parse(localStorage.getItem("TODOs"));
-  lista[i].title = document.getElementById("title").value;
-  lista[i].description = document.getElementById("description").value;
+  var lista = getLocalStorageWithTodos();
+  lista[i].title = $("#title").val();
+  lista[i].description = $("#description").val();
   var checkboxData = getCheckBoxData();
-  if (checkboxData == null) checkboxData = "brak";
+  if (checkboxData == null) checkboxData = "";
   lista[i].important = checkboxData;
   lista[i].type = checkRadio("TODOtype");
-  localStorage.setItem("TODOs", JSON.stringify(lista));
+  updateLocalStorageWithTodos(lista);
   clearFields();
-  displayTODOs(lista[i].userLogin);
   hideFullScrAlert("#fullScrWraper");
+  createAlert("success", "TODO number " + lista[i].id + " updated");
+  const allTodos = new Todo(lista[i].userLogin);
+  allTodos.dislayUserTodos();
+  $("#searchBar").val("");
 }
-function deleteTODO(i) {
+function changeCheckboxValue(important) {
+  switch (important) {
+    case "":
+      break;
+    case "#homework ":
+      console.log("2");
+      $("[name=TODOtype2]")[0].checked = true;
+      break;
+    case "#test ":
+      $("[name=TODOtype2]")[1].checked = true;
+      break;
+    case "#homework #test ":
+      $("[name=TODOtype2]")[0].checked = true;
+      $("[name=TODOtype2]")[1].checked = true;
+      break;
+  }
+}
+function deleteTODO(todoNumber) {
+  var object = findTodoWithId(todoNumber);
   var dane =
     "<span class='bigText'>Are you sure you want to delete TODO ?</span> <br>";
   dane +=
     "<button id='acceptDeletion' class='submitButton' onclick='confirmedDelete(" +
-    i +
+    object.todoPos +
     ")'>yes</button><button id='notAcceptDeletion' class='cancelButton'>no</button>";
   fullScreenAlert(dane, "#fullScrWraper");
 }
 function confirmedDelete(i) {
-  var lista = JSON.parse(localStorage.getItem("TODOs"));
-  var login = lista[i].userLogin;
+  var lista = getLocalStorageWithTodos();
+  var allUserTOdos = new Todo(lista[i].userLogin);
   var number = lista[i].id;
   if (lista.length != 1) {
     lista.splice(i, 1);
   } else lista.pop();
-  localStorage.setItem("TODOs", JSON.stringify(lista));
+  updateLocalStorageWithTodos(lista);
   createAlert("success", "TODO number " + number + " deleted");
-  displayTODOs(login);
-}
-
-function searchForTODO(login) {
-  var el = document.getElementById("tresc");
-  var lista = JSON.parse(localStorage.getItem("TODOs"));
-  if (lista == null) {
-    el.innerHTML =
-      "<tr><td>There is nothing here ... Add a new TODO!</td></tr>";
-    return false;
-  }
-
-  var input = document.getElementById("s").value;
-  var str = "";
-  const regex = new RegExp(input);
-  for (let i = 0; i < lista.length; i++) {
-    if (lista[i].userLogin == login) {
-      if (regex.test(lista[i].title) || regex.test(lista[i].description)) {
-        str +=
-          "<tr><td><h4>TODO id " +
-          i +
-          "   <button onclick='deleteTODO(" +
-          i +
-          ")'> usun TODO</button> " +
-          "  <button onclick='editTODO(" +
-          i +
-          ")'> edytuj TODO</button>" +
-          "  <button onclick='saveEditedTODO(" +
-          i +
-          ")' id='editButton" +
-          i +
-          "' style='visibility :hidden'> zapisz edytowane todo</button></h4></td></tr>";
-        str += "<tr><td> Tytuł :</td><td> " + lista[i].title + "</td></tr>";
-        str +=
-          "<tr><td> Opis  :</td><td>" + lista[i].description + "</td></tr>";
-      }
-    }
-  }
-  if (str == "") el.innerHTML = "Nic tu nie ma .. Dodaj nowe TODO ! ";
-  else el.innerHTML = str;
-
-  document.getElementById("s").value = "";
+  allUserTOdos.dislayUserTodos();
 }
 
 function checkRadio(radioName) {
-  var object = document.getElementsByName(radioName);
-  for (i = 0; i < object.length; i++) {
+  var object = $("[name=" + radioName + "]");
+  for (let i = 0; i < object.length; i++) {
     var choosen = object[i].checked;
     if (choosen) {
       return object[i].value;
@@ -194,9 +222,36 @@ function checkRadio(radioName) {
 
 function getCheckBoxData() {
   var data = "";
-  var object = document.getElementsByName("TODOtype2");
-  for (i = 0; i < object.length; i++) {
+  var object = $('[name="TODOtype2"]');
+  for (let i = 0; i < object.length; i++) {
     if (object[i].checked) data += object[i].value + " ";
   }
   return data;
+}
+
+function printTodo(lista) {
+  var str = "<table class='todoRow'><tr id='last'><td><h5";
+  if (lista.type == "Critical") str += " style='background-color:yellow'>";
+  else str += ">";
+  str += `
+      TODO number  <span class="todoText bigText">${lista.id} </span> </h5>
+      <span class='boxTODO' >
+        <button onclick="deleteTODO(${lista.id})" class='cancelButton'> delete TODO</button>
+        <button onclick="editTODO(${lista.id})">edit TODO</button>
+      </span>
+      </td>
+      </tr>
+      <tr>
+        <td> Title : <span class='todoText'> ${lista.title}</span></td>
+      </tr>
+      <tr>
+        <td> Description  :  <span class='todoText'>${lista.description} </span></td>
+      </tr>
+      <tr>
+        <td> Todo type :  <span class='todoText'>${lista.type}</span></td>
+      </tr>
+      <tr><td> Tags :  <span class='todoText'>${lista.important}</span></td>
+      </tr>
+    </table>`;
+  return str;
 }
